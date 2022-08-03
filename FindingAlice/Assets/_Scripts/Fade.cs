5635a904fe3ad7e95a39bce52949883c23b2d031
gehfand729 <63352794+gehfand729@users.noreply.github.com>
2022-08-03 14:35:41 +0900

    temp

---
+++ FindingAlice/Assets/_Scripts/Fade.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    //페이드 인, 아웃
+    // 페이드 인 아웃시 이미지 알파값 조정
+    // 코루틴
+    private Image fadeImage;
+    private float time, fadeTime, start, end;
+
+    private void Init()
+    {
+        fadeImage = GetComponent<Image>();
+        time = 0;
+        fadeTime = 1;
+        start = 0;
+        end = 1;
+    }
+    
+    private void Awake()
+    {
+        Init();
+    }
+    
+    public void FadeInOut()
+    {
+        StopCoroutine(FadeFlow());
+        StartCoroutine(FadeFlow());
+    }
+
+    private IEnumerator FadeFlow()
+    {
+        Color color = fadeImage.color;
+        while(color.a < end)
+        {
+            time += Time.deltaTime / fadeTime;
+            color.a = Mathf.Lerp(start, end, time);
+            fadeImage.color = color;
+            yield return null;
+        }
+        
+        time = 0;
+
+        while(color.a > start)
+        {
+            time += Time.deltaTime / fadeTime;
+            color.a = Mathf.Lerp(end, start, time);
+            fadeImage.color = color;
+            yield return null;
+        }
+        yield return null;
+        fadeImage.gameObject.SetActive(false);
+
+    }
+
+}
--- FindingAlice/Assets/_Scripts/Manager/PlayerManager.cs
+++ FindingAlice/Assets/_Scripts/Manager/PlayerManager.cs
@@ -15,11 +15,12 @@ public class PlayerManager : MonoBehaviour
     public Vector3 lastCPPos = new Vector3(105, 67, 0);
 
     public bool isGameOver;
-    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private GameObject FadeImage;
 
     private void Awake()
     {
         Init();
+
     }
 
     private void Start()
@@ -29,37 +30,46 @@ public class PlayerManager : MonoBehaviour
             _instance = this;
         }
     }
-    
-    private void Update()
-    {
-        CheckGameOver();
-    }
-    private void Init()
-    {
-        isGameOver = false;
-        // GameObject.FindWithTag("Player").transform.position = lastCPPos;
-    }
 
-    private void CheckGameOver()
+    private void Update()
     {
         if(isGameOver)
         {
-            gameOverPanel.SetActive(true);
-            // DataController.Instance._gameData.playerPosition = lastCPPos;
+            FadeImage.SetActive(true);
+            FadeImage.GetComponent<Fade>().FadeInOut();
+            isGameOver = false;
+            GameObject.FindWithTag("Player").transform.position =
+            DataController.Instance.gameData.playerPosition;
+            // FadeImage.SetActive(false);
         }
     }
 
-    public void RestartScene()
+    private void Init()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        isGameOver = false;
     }
 
-    // 체크포인트 부활시 가져올 정보
-    public void LoadCheckPoint()
-    {
-        GameObject.FindWithTag("Player").transform.position =
-            DataController.Instance.gameData.playerPosition;
-        isGameOver = false;
-        gameOverPanel.SetActive(false);
-    }
+//     private void CheckGameOver()
+//     {
+//         // fade out 은 Update에서 계속 불러주지만 isGameOver가 false가 되면 LoadCheckPoint는 한번만 호출이 되었기 때문.
+//         if(isGameOver)
+//         {
+//             FadeImage.SendMessage("FadeOut");
+//             isGameOver = false;
+//         }
+//         else
+//         {
+//             return;
+//         }
+//     }
+// public void LoadCheckPoint()
+//     {
+//         GameObject.FindWithTag("Player").transform.position =
+//             DataController.Instance.gameData.playerPosition;
+//         isGameOver = false;
+//         FadeImage.GetComponent<Fade>().SendMessage("FadeIn");
+//         // FadeImage.GetComponent<Fade>().FadeIn();
+//         // if(FadeImage.GetComponent<Fade>().endFade)
+//         //     FadeImage.SetActive(false);
+//     }
 }
