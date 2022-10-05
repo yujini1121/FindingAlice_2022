6cd8c341f0937b990df491f7f29d9669c860784b
gehfand729 <gehfand729@gmail.com>
2022-10-05 21:05:08 +0900

    일부 위치 수정
    모든 플랫폼 z축 조정 및 플랫폼 위치 조정

--- FindingAlice/Assets/_Scripts/Platform/ButtonTrigger.cs
+++ FindingAlice/Assets/_Scripts/Platform/ButtonTrigger.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class ButtonTrigger : MonoBehaviour
 {
+    Vector3 rot;
+
+    private void Start()
+    {
+        //rot = this.gameObject.transform.Rotate(this, this, this);
+    }
+
     private void OnTriggerEnter(Collider other) {
         if(other.CompareTag("Player")){
             GetComponentInParent<Transparent>().require = true;
+            //transform.rotation = Quaternion.Euler(x, 180, z);
         }
     }
 }
