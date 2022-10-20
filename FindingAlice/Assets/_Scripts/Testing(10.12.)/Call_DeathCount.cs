e4a8136da195dd76632db65a06749fcefcaf5639
gehfand729 <mo8150h@gmail.com>
2022-10-20 16:43:41 +0900

    UI 적용 및 스크립트 수정
    -로딩 씬에 시계 이미지 적용
    -ClockCounter 시계에 사슬이 감싼 이미지 적용
    -NPC 오브젝트에 Collider 중첩된 부분 수정
    -deathCounter 정적 변수 선언

--- FindingAlice/Assets/_Scripts/Testing(10.12.)/Call_DeathCount.cs
+++ FindingAlice/Assets/_Scripts/Testing(10.12.)/Call_DeathCount.cs
@@ -6,6 +6,7 @@ public class Call_DeathCount : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Áö±İ±îÁöÀÇ ÃÑ »ç¸Á È½¼ö´Â " + GameObject.Find("Player").GetComponentInChildren<PlayerMovement>().deathCounter + "¹ø Á×À¸¼Ì½À´Ï´Ù.");
+        //Debug.Log("Áö±İ±îÁöÀÇ ÃÑ »ç¸Á È½¼ö´Â " + GameObject.Find("Player").GetComponentInChildren<PlayerMovement>().deathCounter + "¹ø Á×À¸¼Ì½À´Ï´Ù.");
+        Debug.Log("Áö±İ±îÁöÀÇ ÃÑ »ç¸Á È½¼ö´Â " + PlayerMovement.deathCounter + "¹ø Á×À¸¼Ì½À´Ï´Ù.");
     }
 }
