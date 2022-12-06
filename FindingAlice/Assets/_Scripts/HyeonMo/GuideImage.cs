139553d75cb9ad7242f685266818876c8f1fc9ba
HyeonMoKim <109887733+HyeonMoKim0@users.noreply.github.com>
2022-12-06 18:02:20 +0900

    수정

--- FindingAlice/Assets/_Scripts/HyeonMo/GuideImage.cs
+++ FindingAlice/Assets/_Scripts/HyeonMo/GuideImage.cs
@@ -1,45 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GuideImage : MonoBehaviour
 {
-    int childCount;
+    int childCount = 0;
 
-    GameObject parent;
-    GameObject[] child;
+    //Inspector에서 키려는 GuideImages 오브젝트 할당
+    [SerializeField] GameObject parentGuideImages;
 
+    [SerializeField] bool[] nextButtons;
+
+    //기본적으로 SetActive(false)로 할 것
     [SerializeField] GameObject[] guideImages;
+
+    [SerializeField] Image[] images;
+    [SerializeField] Button[] buttons;
+
+    void Awake()
+    {
+        childCount = parentGuideImages.transform.childCount;
+    }
 
     void Start()
     {
-        parent = this.gameObject;
-        childCount = this.transform.childCount;
+        //childCount = parentGuideImages.transform.childCount;
 
-
+        guideImages = new GameObject[childCount];
+        images = new Image[childCount];
+        buttons = new Button[childCount];
+        nextButtons = new bool[childCount];
 
         for (int i = 0; i < childCount; i++)
         {
-            guideImages[i] = transform.GetChild(i).gameObject;
+            guideImages[i] = parentGuideImages.transform.GetChild(i).gameObject;
 
-            guideImages[i].SetActive(false);
+            images[i] = guideImages[i].GetComponent<Image>();
+            buttons[i] = guideImages[i].GetComponent<Button>();
+
+            //guideImages[i].SetActive(false);
         }
-
-        //foreach (int count in childCount)
-        //{
-
-        //}
-
-
-
-
-
-
     }
 
-
-    void Update()
+    public void OffImages()
     {
-        
+        for (int i = childCount - 1; i >= 0; i++)
+        {
+            if (i <= 0)
+                break;
+
+            if (!guideImages[i].activeSelf)
+                continue;
+
+            //guideImages[i - 1].SetActive(true);
+            guideImages[i].SetActive(false);
+            StartCoroutine(TurnOnGuide());
+            break;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            guideImages[childCount - 1].SetActive(true);
+
+            StartCoroutine(TurnOnGuide());
+        }
+    }
+
+    IEnumerator TurnOnGuide()
+    {
+        yield return new WaitForSeconds(3.0f);
+
+        for (int i = childCount - 1; i >= 0; i--)
+        {
+            if (i <= 0)
+                break;
+
+            if (nextButtons[i])
+                continue;
+
+            buttons[i].enabled = true;
+            nextButtons[i] = true;
+
+            break;
+        }
+
+        yield break;
     }
 }
--- FindingAlice/Assets/_Scripts/HyeonMo/TurnOnGuideImage.cs
+++ FindingAlice/Assets/_Scripts/HyeonMo/TurnOnGuideImage.cs
@@ -5,60 +5,74 @@ using UnityEngine.UI;
 
 public class TurnOnGuideImage : MonoBehaviour
 {
-    int parentChildCount;
+    //해당 스크립트는 Guide관련 모든 오브젝트에 할당해야 합니다.
+
+    int childCount = 0;
+
+    [SerializeField] GameObject parentGuideImages; 
+    //위의 변수에는 Inspector에서 키려는 GuideImages 오브젝트 할당
+    
 
     [SerializeField] bool[] nextButtons;
 
     [SerializeField] GameObject[] guideImages;
 
     [SerializeField] Image[] images;
-    [SerializeField] Button[] buttons;
-    
+    [SerializeField] Button[] buttons; //응쓰게싸
+
+    //void Awake()
+    //{
+    //    if (parentGuideImages == null)
+    //    {
+    //        if (this.transform.GetComponent<RectTransform>())
+    //        {
+    //            parentGuideImages = this.transform.gameObject;
+    //            Debug.Log(this.gameObject.name + "정상");
+
+    //            if (parentGuideImages.transform.GetChild(0) == null)
+    //                parentGuideImages = parentGuideImages.transform.parent.gameObject;
+    //        }
+
+    //        else if (this.transform.GetComponent<Transform>())
+    //            Debug.Log(this.transform.name + "이라는 오브젝트에 GuideImage가 할당되지 않았습니다.");
+    //    }
+    //}
+
     void Start()
     {
-        parentChildCount = GameObject.Find("GuideImages").transform.childCount;
+        childCount = parentGuideImages.transform.childCount;
 
-        guideImages = new GameObject[parentChildCount];
-        images = new Image[parentChildCount];
-        buttons = new Button[parentChildCount];
-        nextButtons = new bool[parentChildCount];
+        guideImages = new GameObject[childCount];
+        images = new Image[childCount];
+        buttons = new Button[childCount];
+        nextButtons = new bool[childCount];
 
-        for (int i = 0; i < parentChildCount; i++)
+        //Debug.Log(this.gameObject.name + "Start 정상");
+
+        for (int i = 0; i < childCount; i++)
         {
-            guideImages[i] = GameObject.Find("GuideImages").transform.GetChild(i).gameObject;
-            guideImages[i].SetActive(false);
-        }
-        
-        for (int i = 0; i < parentChildCount; i++)
-        {
+            //Debug.Log(this.gameObject.name + "for문 시작 전");
+            guideImages[i] = parentGuideImages.transform.GetChild(i).gameObject;
+            
             images[i] = guideImages[i].GetComponent<Image>();
             buttons[i] = guideImages[i].GetComponent<Button>();
 
             buttons[i].enabled = false;
             nextButtons[i] = false;
-        }
-    }
+            guideImages[i].SetActive(false);
 
-    void Update()
-    {
-        for (int i = parentChildCount - 2; i >= 0; i--)
-        {
-            if (!guideImages[i + 1].activeSelf)
-            {
-                guideImages[i].SetActive(true);
-                break;
-            }
-            break;
-            
+            //Debug.Log(this.gameObject.name + "for문 정상 실행, (" + i +")번째");
         }
-
 
     }
 
     public void OffImages()
     {
-        for (int i = 0; i < parentChildCount; i++)
+        for (int i = childCount - 1; i >= 0; i++)
         {
+            if (i <= 0)
+                break;
+
             if (!guideImages[i].activeSelf)
                 continue;
             
@@ -68,38 +82,45 @@ public class TurnOnGuideImage : MonoBehaviour
         }
     }
 
-    //void OnDisable()
-    //{
-        
-    //}
-
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            //for (int i = parentChildCount - 1; i >= 0; i--)
-            //{
-            //    guideImages[i].SetActive(true);
-            //}
-
-            guideImages[parentChildCount - 1].SetActive(true);
+            guideImages[childCount - 1].SetActive(true);
 
             StartCoroutine(TurnOnGuide());
         }
     }
 
+    //void OnDisable()
+    //{
+    //    for (int i = childCount - 1; i >= 0; i--)
+    //    {
+    //        if (i <= 0)
+    //            break;
+
+    //        if (this.transform.gameObject == guideImages[i])
+    //        {
+    //            guideImages[i - 1].SetActive(true);
+    //            break;
+    //        }
+    //    }
+    //}
+
     IEnumerator TurnOnGuide()
     {
         yield return new WaitForSeconds(3.0f);
 
-        buttons[parentChildCount - 1].enabled = true;
-        nextButtons[parentChildCount - 1] = true;
+        buttons[childCount - 1].enabled = true;
+        nextButtons[childCount - 1] = true;
 
-        for (int i = parentChildCount; i >= 0; i--)
+        for (int i = childCount - 1; i >= 0; i--)
         {
+            if (i <= 0)
+                break;
+
             if (nextButtons[i])
                 continue;
-
 
             buttons[i].enabled = true;
             nextButtons[i] = true;
