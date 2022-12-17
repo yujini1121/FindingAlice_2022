34dc24320e4198e44c029f83039bfbca8b80fc08
HyeonMoKim0 <mo8150h@gmail.com>
2022-12-18 05:29:24 +0900

    추가점
    -실험하다 버려진 스크립트 제거
    -ScreenFixed 스크립트 내용 일부 변경
    -ScreenFixed 스크립트 이름을 FixImageSize로 변경

---
+++ FindingAlice/Assets/_Scripts/HyeonMo/UI/FixImageSize.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FixImageSize : MonoBehaviour
+{
+    //가이드 이미지와 GuideImage 오브젝트의 Width, Height는 같아야 함
+    float defaultWidth  ,   defaultHeight;
+    float deviceWidth   ,   deviceHeight;
+
+    float ImageWidth,   ImageHeight;   // 해당 이미지 너비와 높이
+
+    float deviceAspectRatio;
+    float defaultAspectRatio;
+    float ImageAspectRatio;
+
+    Vector2 rect_SizeDelta;
+
+    void Awake()
+    {
+        GameObject canvas = GameObject.Find("Canvas");
+
+        rect_SizeDelta = transform.gameObject.GetComponent<RectTransform>().sizeDelta;
+
+        defaultWidth = canvas.gameObject.GetComponent<CanvasScaler>().referenceResolution.x;
+        defaultHeight = canvas.gameObject.GetComponent<CanvasScaler>().referenceResolution.y;
+        deviceWidth = Screen.width;
+        deviceHeight = Screen.height;
+        ImageWidth = rect_SizeDelta.x;
+        ImageHeight = rect_SizeDelta.y;
+
+        deviceAspectRatio = deviceWidth / deviceHeight;
+        defaultAspectRatio = defaultWidth / defaultHeight;
+        ImageAspectRatio = ImageWidth / ImageHeight;
+    }
+
+    void OnEnable()
+    {
+        SetResolution();
+    }
+
+    public void SetResolution()
+    {
+        if (ImageAspectRatio > deviceAspectRatio)
+        {
+            Debug.Log("way1");
+            rect_SizeDelta = new Vector2(
+                (int)(ImageWidth * (1.0 + defaultAspectRatio * 0.1)),
+                (int)(ImageHeight * (1.0 + defaultAspectRatio * 0.1)));
+        }
+        else if (ImageAspectRatio < deviceAspectRatio)
+        {
+            Debug.Log("way2");
+            rect_SizeDelta = new Vector2(
+                (int)(ImageWidth * (1.0 - defaultAspectRatio * 0.1)),
+                (int)(ImageHeight * (1.0 - defaultAspectRatio * 0.1)));
+        }
+    }
+}
